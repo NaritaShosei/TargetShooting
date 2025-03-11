0646a48031588fd8d6f1c9d9ee326baf7ea56a6f
NaritaShosei <onigirikun7171@gmail.com>
2025-03-11 16:34:32 +0900

    [update]パネルが動く処理

--- Assets/Script/PanelManager.cs
+++ Assets/Script/PanelManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class PanelManager : MonoBehaviour
@@ -8,12 +9,25 @@ public class PanelManager : MonoBehaviour
     [SerializeField]
     List<ScorePanel> _panels = new();
     Dictionary<ScorePanel, bool> _dic = new();
+    [SerializeField]
+    List<Transform> _targets = new();
+    int _index;
+    float _moveSpeed = 1;
     void Start()
     {
         Instance = this;
         foreach (var item in _panels)
         {
             _dic.Add(item, true);
+        }
+    }
+    private void Update()
+    {
+        var dir = _targets[_index].position - transform.position;
+        transform.position += dir.normalized * _moveSpeed * Time.deltaTime;
+        if ((_targets[_index].position - transform.position).magnitude < 0.3f)
+        {
+            _index = (_index + 1) % _targets.Count;
         }
     }
     public bool ValueChange(ScorePanel panel)
